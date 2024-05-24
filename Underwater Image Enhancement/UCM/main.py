467f7457db0ffc5b5fdeaff4cef456d36af1f241
Andreas Wendelboe <andreaswendelboe.dk@gmail.com>
2024-05-24 11:14:04 +0200

    Histogram sort once and all vectorized

--- Underwater Image Enhancement/UCM/main.py
+++ Underwater Image Enhancement/UCM/main.py
@@ -4,119 +4,116 @@ import cv2
 import natsort
 import xlwt
 import datetime
-from skimage.color import rgb2hsv,hsv2rgb
+from skimage.color import rgb2hsv, hsv2rgb
 
-np.seterr(over='ignore')
-if __name__ == '__main__':
+np.seterr(over="ignore")
+if __name__ == "__main__":
     pass
 
 # Get dataset
 folder = "/home/aw/Code/UVision/image_processing/image_enhancement/Dataset"
 path = folder + "/input"
 files = os.listdir(path)
-files = [file for file in files if file != '.DS_Store'] # Filter out '.DS_Store' files
-files =  natsort.natsorted(files)
+files = [file for file in files if file != ".DS_Store"]  # Filter out '.DS_Store' files
+files = natsort.natsorted(files)
+
 
 ###
 ### color_equalisation.py
 ###
-def cal_equalisation(img,ratio):
+def cal_equalisation(img, ratio):
     Array = img * ratio
     Array = np.clip(Array, 0, 255)
     return Array
+
 
 def RGB_equalisation(img):
     img = np.float32(img)
     avg_RGB = []
     for i in range(3):
-        avg = np.mean(img[:,:,i])
+        avg = np.mean(img[:, :, i])
         avg_RGB.append(avg)
     # print('avg_RGB',avg_RGB)
-    a_r = avg_RGB[0]/avg_RGB[2]
-    a_g =  avg_RGB[0]/avg_RGB[1]
-    ratio = [0,a_g,a_r]
-    for i in range(1,3):
-        img[:,:,i] = cal_equalisation(img[:,:,i],ratio[i])
+    a_r = avg_RGB[0] / avg_RGB[2]
+    a_g = avg_RGB[0] / avg_RGB[1]
+    ratio = [0, a_g, a_r]
+    for i in range(1, 3):
+        img[:, :, i] = cal_equalisation(img[:, :, i], ratio[i])
     return img
+
 
 ##
 ## global_histogram_stretching.py
 ##
-def histogram_r(r_array, height, width):
-    length = height * width
-    R_rray = r_array.flatten()
-    R_rray.sort()
-    I_min = int(R_rray[int(length / 500)])
-    I_max = int(R_rray[-int(length / 500)])
-    
+def histogram_r(r_array, I_min, I_max):
     array_Global_histogram_stretching = np.zeros_like(r_array)
     mask1 = r_array < I_min
     mask2 = r_array > I_max
     array_Global_histogram_stretching[mask1] = I_min
     array_Global_histogram_stretching[mask2] = 255
     mask3 = ~(mask1 | mask2)
-    array_Global_histogram_stretching[mask3] = ((r_array[mask3] - I_min) * ((255 - I_min) / (I_max - I_min))) + I_min
-    
+    array_Global_histogram_stretching[mask3] = (
+        (r_array[mask3] - I_min) * ((255 - I_min) / (I_max - I_min))
+    ) + I_min
     return array_Global_histogram_stretching
 
-def histogram_g(r_array,height, width):
-    length = height * width
-    R_rray = []
-    for i in range(height):
-        for j in range(width):
-            R_rray.append(r_array[i][j])
-    R_rray.sort()
-    I_min = int(R_rray[int(length / 500)])
-    I_max = int(R_rray[-int(length / 500)])
-    array_Global_histogram_stretching = np.zeros((height, width))
-    for i in range(0, height):
-        for j in range(0, width):
-            if r_array[i][j] < I_min:
-                p_out = r_array[i][j]
-                array_Global_histogram_stretching[i][j] = 0
-            elif (r_array[i][j] > I_max):
-                p_out = r_array[i][j]
-                array_Global_histogram_stretching[i][j] = 255
-            else:
-                p_out = int((r_array[i][j] - I_min) * ((255) / (I_max - I_min)) )
-                array_Global_histogram_stretching[i][j] = p_out
-    return (array_Global_histogram_stretching)
 
-def histogram_b(r_array,height, width):
-    length = height * width
-    R_rray = []
-    for i in range(height):
-        for j in range(width):
-            R_rray.append(r_array[i][j])
-    R_rray.sort()
-    I_min = int(R_rray[int(length / 500)])
-    I_max = int(R_rray[-int(length / 500)])
-    array_Global_histogram_stretching = np.zeros((height, width))
-    for i in range(0, height):
-        for j in range(0, width):
-            if r_array[i][j] < I_min:
-                # p_out = r_array[i][j]
-                array_Global_histogram_stretching[i][j] = 0
-            elif (r_array[i][j] > I_max):
-                # p_out = r_array[i][j]
-                array_Global_histogram_stretching[i][j] = I_max
-            else:
-                p_out = int((r_array[i][j] - I_min) * ((I_max) / (I_max - I_min)))
-                array_Global_histogram_stretching[i][j] = p_out
-    return (array_Global_histogram_stretching)
+def histogram_g(r_array, I_min, I_max):
+    array_Global_histogram_stretching = np.zeros_like(r_array)
+    mask1 = r_array < I_min
+    mask2 = r_array > I_max
+    array_Global_histogram_stretching[mask1] = 0
+    array_Global_histogram_stretching[mask2] = 255
+    mask3 = ~(mask1 | mask2)
+    array_Global_histogram_stretching[mask3] = (r_array[mask3] - I_min) * (
+        (255) / (I_max - I_min)
+    )
+    return array_Global_histogram_stretching
+
+
+def histogram_b(r_array, I_min, I_max):
+    array_Global_histogram_stretching = np.zeros_like(r_array)
+    mask1 = r_array < I_min
+    mask2 = r_array > I_max
+    array_Global_histogram_stretching[mask1] = 0
+    array_Global_histogram_stretching[mask2] = I_max
+    mask3 = ~(mask1 | mask2)
+    array_Global_histogram_stretching[mask3] = (r_array[mask3] - I_min) * (
+        (I_max) / (I_max - I_min)
+    )
+    return array_Global_histogram_stretching
+
 
 def stretching(img):
-    height = len(img)
-    width = len(img[0])
-    img[:, :, 2] = histogram_r(img[:, :, 2], height, width)
-    img[:, :, 1] = histogram_g(img[:, :, 1], height, width)
-    img[:, :, 0] = histogram_b(img[:, :, 0], height, width)
+    height, width, _ = img.shape
+    length = height * width
+
+    R_rray = img[:, :, 2].flatten()
+    R_rray.sort()
+    I_min_r = int(R_rray[int(length / 500)])
+    I_max_r = int(R_rray[-int(length / 500)])
+
+    G_rray = img[:, :, 1].flatten()
+    G_rray.sort()
+    I_min_g = int(G_rray[int(length / 500)])
+    I_max_g = int(G_rray[-int(length / 500)])
+
+    B_rray = img[:, :, 0].flatten()
+    B_rray.sort()
+    I_min_b = int(B_rray[int(length / 500)])
+    I_max_b = int(B_rray[-int(length / 500)])
+
+    img[:, :, 2] = histogram_r(img[:, :, 2], I_min_r, I_max_r)
+    img[:, :, 1] = histogram_g(img[:, :, 1], I_min_g, I_max_g)
+    img[:, :, 0] = histogram_b(img[:, :, 0], I_min_b, I_max_b)
+
     return img
+
 
 ##
 ## hsvStretching.py
 ##
-def  HSVStretching(sceneRadiance):
+def HSVStretching(sceneRadiance):
     sceneRadiance = np.uint8(sceneRadiance)
     height = len(sceneRadiance)
     width = len(sceneRadiance[0])
@@ -125,7 +122,7 @@ def  HSVStretching(sceneRadiance):
     img_s_stretching = global_stretching(s, height, width)
     img_v_stretching = global_stretching(v, height, width)
 
-    labArray = np.zeros((height, width, 3), 'float64')
+    labArray = np.zeros((height, width, 3), "float64")
     labArray[:, :, 0] = h
     labArray[:, :, 1] = img_s_stretching
     labArray[:, :, 2] = img_v_stretching
@@ -135,14 +132,14 @@ def  HSVStretching(sceneRadiance):
 
     return img_rgb
 
+
 ##
 ## global_Stretching.py
-## 
-def global_stretching(img_L,height, width):
+##
+def global_stretching(img_L, height, width):
     I_min = np.min(img_L)
     I_max = np.max(img_L)
     I_mean = np.mean(img_L)
-
 
     # print('I_min',I_min)
     # print('I_max',I_max)
@@ -155,6 +152,7 @@ def global_stretching(img_L,height, width):
             array_Global_histogram_stretching_L[i][j] = p_out
 
     return array_Global_histogram_stretching_L
+
 
 ##
 ## sceneRadiance.py
@@ -174,18 +172,20 @@ startTimeTotal = datetime.datetime.now()
 for i in range(len(files)):
     file = files[i]
     filepath = path + "/" + file
-    prefix = file.split('.')[0]
+    prefix = file.split(".")[0]
     if os.path.isfile(filepath):
         start_time = datetime.datetime.now()
-        img = cv2.imread(folder + '/input/' + file)
+        img = cv2.imread(folder + "/input/" + file)
         sceneRadiance = RGB_equalisation(img)
         sceneRadiance = stretching(sceneRadiance)
         sceneRadiance = HSVStretching(sceneRadiance)
         sceneRadiance = sceneRadianceRGB(sceneRadiance)
-        cv2.imwrite(folder + '/output/' + prefix + '_UCM.jpg', sceneRadiance)
+        cv2.imwrite(folder + "/output/" + prefix + "_UCM.jpg", sceneRadiance)
         end_time = datetime.datetime.now()
-        print('********    File: ',file, ' Time: ', end_time - start_time, '    ********')
+        print(
+            "********    File: ", file, " Time: ", end_time - start_time, "    ********"
+        )
 
 endTimeTotal = datetime.datetime.now()
-time = endTimeTotal-startTimeTotal
-print('time',time)
+time = endTimeTotal - startTimeTotal
+print("time", time)
