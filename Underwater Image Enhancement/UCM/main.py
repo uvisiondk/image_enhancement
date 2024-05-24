37744ed956a5e5aef726f6ff01d6097dc5972079
Andreas Wendelboe <andreaswendelboe.dk@gmail.com>
2024-05-24 10:54:00 +0200

    First initial run

--- Underwater Image Enhancement/UCM/main.py
+++ Underwater Image Enhancement/UCM/main.py
@@ -10,34 +10,34 @@ from global_histogram_stretching import stretching
 from hsvStretching import HSVStretching
 from sceneRadiance import sceneRadianceRGB
 
-
 np.seterr(over='ignore')
 if __name__ == '__main__':
     pass
 
-# folder = "C:/Users/Administrator/Desktop/UnderwaterImageEnhancement/NonPhysical/UCM"
-folder = "C:/Users/Administrator/Desktop/Databases/Dataset"
-
-path = folder + "/InputImages"
+# Get dataset
+folder = "/home/aw/Code/UVision/image_processing/image_enhancement/Dataset"
+path = folder + "/input"
 files = os.listdir(path)
+files = [file for file in files if file != '.DS_Store'] # Filter out '.DS_Store' files
 files =  natsort.natsorted(files)
 
+
+startTimeTotal = datetime.datetime.now()
 for i in range(len(files)):
     file = files[i]
     filepath = path + "/" + file
     prefix = file.split('.')[0]
     if os.path.isfile(filepath):
-        print('********    file   ********',file)
-        # img = cv2.imread('InputImages/' + file)
-        img = cv2.imread(folder + '/InputImages/' + file)
-        # print('Number',Number)
+        start_time = datetime.datetime.now()
+        img = cv2.imread(folder + '/input/' + file)
         sceneRadiance = RGB_equalisation(img)
         sceneRadiance = stretching(sceneRadiance)
-        # # cv2.imwrite(folder + '/OutputImages/' + Number + 'Stretched.jpg', sceneRadiance)
         sceneRadiance = HSVStretching(sceneRadiance)
         sceneRadiance = sceneRadianceRGB(sceneRadiance)
         cv2.imwrite('OutputImages/' + prefix + '_UCM.jpg', sceneRadiance)
+        end_time = datetime.datetime.now()
+        print('********    File: ',file, ' Time: ', end_time - start_time, '    ********')
 
-endtime = datetime.datetime.now()
-time = endtime-starttime
+endTimeTotal = datetime.datetime.now()
+time = endTimeTotal-startTimeTotal
 print('time',time)
